e0b777d4818a3b594cd4b8c9944ae1af33a37a97
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-06-24 09:03:47 +0900

    tirrilee test 0624

--- fe/src/webapp/estimatedProgramm/container/ProgrammApp.js
+++ fe/src/webapp/estimatedProgramm/container/ProgrammApp.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import HomeBody from './HomeBody';
-import NavBar from './NavBar';
 import 'webapp/estimatedProgramm/css/ProgrammMainCss.css'
 
 const ProgrammApp = () => {
     return (<>
-    <NavBar/>
     <HomeBody/>
     </>);
 }
